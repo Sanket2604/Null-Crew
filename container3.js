6977989f7d37eb8df918ee6dcd68076c3f76a4bf
Sanket Banerjee <umaymailme2604@gmail.com>
2020-09-07 15:33:25 +0530

    5th commit

---
+++ container3.js
@@ -0,0 +1,40 @@
+function sizereduce1(){
+    document.querySelector('.image1').style.height = '0px';
+    document.querySelector('.hidden1').style.display = 'block';
+}
+function sizenormal1(){
+    document.querySelector('.image1').style.height = '60px';
+    document.querySelector('.hidden1').style.display = 'none';
+}
+function sizereduce2(){
+    document.querySelector('.image2').style.height = '0px';
+    document.querySelector('.hidden2').style.display = 'block';
+}
+function sizenormal2(){
+    document.querySelector('.image2').style.height = '60px';
+    document.querySelector('.hidden2').style.display = 'none';
+}
+function sizereduce3(){
+    document.querySelector('.image3').style.height = '0px';
+    document.querySelector('.hidden3').style.display = 'block';
+}
+function sizenormal3(){
+    document.querySelector('.image3').style.height = '60px';
+    document.querySelector('.hidden3').style.display = 'none';
+}
+function sizereduce4(){
+    document.querySelector('.image4').style.height = '0px';
+    document.querySelector('.hidden4').style.display = 'block';
+}
+function sizenormal4(){
+    document.querySelector('.image4').style.height = '60px';
+    document.querySelector('.hidden4').style.display = 'none';
+}
+function sizereduce5(){
+    document.querySelector('.image5').style.height = '0px';
+    document.querySelector('.hidden5').style.display = 'block';
+}
+function sizenormal5(){
+    document.querySelector('.image5').style.height = '60px';
+    document.querySelector('.hidden5').style.display = 'none';
+}
